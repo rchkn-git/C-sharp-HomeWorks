f767d747172a7209312b4a046f40e6ff5f891d0d
Aleksandr Ruchkan <aruchkan@yandex.ru>
2023-03-12 15:15:03 +0300

    Zadanie 1 done

---
+++ Homework 1/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a > b)
+{
+    Console.WriteLine("Первое число больше второго!");
+}
+else
+{
+    Console.WriteLine("Второе число больше первого!");
+}
