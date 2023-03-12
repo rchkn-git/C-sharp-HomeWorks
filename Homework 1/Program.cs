b3f87d08f3f10ee116d8b31fc0a7f2209365eb41
Aleksandr Ruchkan <aruchkan@yandex.ru>
2023-03-12 15:23:36 +0300

    Zadacha 2 done

--- Homework 1/Program.cs
+++ Homework 1/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/* Задание 1
 Console.WriteLine("Введите первое число: ");
 int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число: ");
@@ -11,3 +11,28 @@ else
 {
     Console.WriteLine("Второе число больше первого!");
 }
+*/
+
+/* Задание 2
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if (max < b)
+{
+    max = b;
+}
+if (max < c)
+{
+    max = c;
+    Console.WriteLine("Самое большое число из трёх: " + max);
+}
+else
+{
+    Console.WriteLine("Самое большое число из трёх: " + max);
+}
+*/
+
