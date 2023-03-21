c9b8d3e2b23a68f635976f522983aeb941c58692
Aleksandr Ruchkan <aruchkan@yandex.ru>
2023-03-21 22:50:40 +0300

    Homework 5 started

---
+++ Homework 5/Program.cs
@@ -0,0 +1,26 @@
+﻿//генерируем рандомный массив
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+
+    return result;
+}
+
+int CheckChetn(int[] arr)
+{
+    int res = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if ((arr[i] % 2) == 0 && arr[i] != 0) res++;
+        Console.WriteLine (arr[i] + " " + res);
+    }
+    return res; 
+}
+
+int[] array = GetRandomArray(10, 0, 9);
+Console.WriteLine($"[{String.Join(",", array)}]");
+Console.WriteLine(CheckChetn(array));
